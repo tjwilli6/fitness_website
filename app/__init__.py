827f588c4dd929be248a6fd3d6057c4040367538
Tyler Williamson <tjwilli@udel.edu>
2019-02-08 15:09:26 -0500

    Commit before new branch

--- app/__init__.py
+++ app/__init__.py
@@ -2,11 +2,13 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from app.config import Config
 from flask_login import LoginManager
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 login = LoginManager(app)
 login.login_view = 'login'
+bootstrap = Bootstrap(app)
 
 from app import routes,models
