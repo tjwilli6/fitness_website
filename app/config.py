215aab5016f1f355a62aafd879b6f229ddfd5820
Tyler Williamson <tjwilli@udel.edu>
2019-02-08 21:29:03 -0500

    changed config

--- app/config.py
+++ app/config.py
@@ -2,6 +2,8 @@ import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+WT_ERROR = 5.
+
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'not_a_doctor___shhh'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
