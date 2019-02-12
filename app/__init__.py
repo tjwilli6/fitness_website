6545f77708572ed8ada0f73c433027632d6f83cf
Tyler Williamson <tjwilli@udel.edu>
2019-02-12 16:37:05 -0500

    Removed unused module

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,6 @@ from flask_sqlalchemy import SQLAlchemy
 from app.config import Config
 from flask_login import LoginManager
 from flask_bootstrap import Bootstrap
-from flask_images import Images
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -11,6 +10,5 @@ db = SQLAlchemy(app)
 login = LoginManager(app)
 login.login_view = 'login'
 bootstrap = Bootstrap(app)
-images = Images(app)
 
 from app import routes,models
